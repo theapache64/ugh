360288eb93196ce6e53778b2446aa24c72427f9a
theapache64 <theapache64@gmail.com>
2023-10-02 10:56:53 +0530

    .gitignore refreshed

--- src/com/theah64/ugh/utils/StringUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.theah64.ugh.utils
-
-object StringUtils {
-
-    /**
-     * Returns an array of commit command than can be executed with ProcessBuilder
-     */
-    fun getCommitCommand(finalCommitMessage: String) = arrayOf("git", "commit", "-m", finalCommitMessage)
-}
